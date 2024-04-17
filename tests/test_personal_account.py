bc934e9cbd2c70bd10d13202045066fcf0daab2c
Marat Sharipov <msharipov@gopoints.ru>
2024-04-17 21:24:49 +0300

    3 задания диплома

---
+++ tests/test_personal_account.py
@@ -0,0 +1,31 @@
+from locators.locators import MainPageLocators as mp
+from pages.login_page import LoginPage
+from locators.locators import PathRecoveryPages as rp
+import allure
+
+
+class TestPersonalAccount:
+    @allure.description('переход по клику на «Личный кабинет»')
+    def test_click_personal_account(self, browser):
+        login_page = LoginPage(browser)
+        login_page.authorize()
+        login_page.click_on_element(mp.account_autorize_button)
+        login_page.wait_element(mp.check_text_account_pages)
+        assert login_page.get_element_text(mp.check_text_account_pages) == 'В этом разделе вы можете изменить свои персональные данные'
+
+    @allure.description('переход в раздел «История заказов»,')
+    def test_click_order_history(self, browser):
+        login_page = LoginPage(browser)
+        login_page.authorize()
+        login_page.click_on_element(mp.account_autorize_button)
+        login_page.click_on_element(mp.order_history)
+        login_page.wait_element(mp.check_text_order_history)
+        assert login_page.get_element_text(mp.check_text_order_history) in "Выполнен"
+
+    @allure.description('выход из аккаунта.')
+    def test_exit_personal_account(self, browser):
+        login_page = LoginPage(browser)
+        login_page.authorize()
+        login_page.click_on_element(mp.exit_button)
+        login_page.wait_element(rp.check_text_on_page_recovery_password)
+        assert login_page.get_element_text(rp.check_text_on_page_recovery_password) == 'Восстановление пароля'
---
+++ tests/test_recovery_password.py
@@ -0,0 +1,30 @@
+from locators.locators import PathRecoveryPages as rp
+from pages.base_pages import BasePage
+from data.data import UserData as ud
+import allure
+
+class TestCheckRecoveryPasswordPage:
+
+    @allure.description('переход на страницу восстановления пароля по кнопке «Восстановить пароль»')
+    def test_check_recovery_password_page(self, browser):
+        base_page = BasePage(browser)
+        base_page.click_on_element(rp.account_entry)
+        base_page.click_on_element(rp.recovery_password)
+        assert base_page.get_element_text(rp.check_text_on_page_recovery_password) == 'Восстановление пароля'
+
+    @allure.description('ввод почты и клик по кнопке «Восстановить»')
+    def test_input_mail_and_click_button_recovery(self, browser):
+        base_page = BasePage(browser)
+        base_page.click_on_element(rp.account_entry)
+        base_page.click_on_element(rp.recovery_password)
+        base_page.fill_field(rp.input_email_user, ud.email)
+        base_page.click_on_element(rp.recovery_button)
+        assert base_page.get_element_text(rp.check_text_code_recovery_password) == 'Введите код из письма'
+
+    @allure.description('клик по кнопке показать/скрыть пароль делает поле активным — подсвечивает его')
+    def test_click_eye_button(self, browser):
+        base_page = BasePage(browser)
+        base_page.click_on_element(rp.account_entry)
+        base_page.fill_field(rp.input_password_user, ud.password)
+        base_page.click_on_element(rp.eye_button)
+        assert base_page.wait_element(rp.check_activ_eye_button) is not None
